8988e33834e5ec12745da2824977f5cf24b3d51b
Manuel García <115319759+BandiCode@users.noreply.github.com>
2022-12-21 20:57:39 -0300

    Commit

---
+++ optimize-intel.sh
@@ -0,0 +1,14 @@
+sudo add-apt-repository -y ppa:apt-fast/stable 
+sudo apt-get -y update 
+sudo apt-get install -y apt-fast
+sudo apt-get install -y preload
+systemctl enable --now preload
+echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
+wget -qO - https://cdn.starnode.us/archivos_xanmod/gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
+sudo apt update && sudo apt install linux-xanmod-rt-x64v3
+git clone https://github.com/vainkii/kernel-tweaks.git && cd kernel-tweaks
+chmod 750 ./install.sh && bash ./install.sh
+fstrim -a
+sudo apt clean
+sudo apt autoremove
+sudo apt install intel-microcode iucode-tool
