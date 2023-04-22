efe3c47b2f0675df5d0e1dd160b124cc8ccc200c
Manuel (OpticalSEC) <115319759+opticalsecurity@users.noreply.github.com>
2023-04-22 20:42:41 -0300

    Fix XanMod

--- optimize-intel.sh
+++ optimize-intel.sh
@@ -3,9 +3,9 @@ sudo apt-get -y update
 sudo apt-get install -y apt-fast
 sudo apt-get install -y preload
 sudo systemctl enable --now preload
-echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
-wget -qO - https://cdn.starnode.us/archivos_xanmod/gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
-sudo apt update && sudo apt install linux-xanmod-rt-x64v3
+wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
+echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list
+sudo apt update && sudo apt install linux-xanmod-x64v3
 git clone https://github.com/vainkii/kernel-tweaks.git && cd kernel-tweaks
 chmod 750 ./install.sh && bash ./install.sh
 fstrim -a
