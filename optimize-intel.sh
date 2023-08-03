a0100933291219a967543cf4124ddea9a6319d05
nico <118127295+vainki@users.noreply.github.com>
2023-08-03 00:51:24 -0300

    Update optimize-intel.sh

--- optimize-intel.sh
+++ optimize-intel.sh
@@ -6,7 +6,7 @@ sudo systemctl enable --now preload
 wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
 echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list
 sudo apt update && sudo apt install linux-xanmod-x64v3
-git clone https://github.com/vainkii/kernel-tweaks.git && cd kernel-tweaks
+git clone https://github.com/vainki/kernel-tweaks.git && cd kernel-tweaks
 chmod 750 ./install.sh && bash ./install.sh
 fstrim -a
 sudo apt clean
